c75fbb631d35a9a6068adbfdab42da7f6b22eda7
NguyenKhanhAn-Sora <soranguyen1211@gmail.com>
2026-05-19 22:35:15 +0700

    merge main

--- cordigram-mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cordigram-mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import app_links
 import audioplayers_darwin
 import connectivity_plus
 import desktop_webview_window
@@ -27,7 +26,6 @@ import video_player_avfoundation
 import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
